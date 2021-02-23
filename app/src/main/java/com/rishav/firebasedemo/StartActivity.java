74305f22f6e0016fbf35578f4e7db75fc40dacaa
Leroytheone <leroy673412@gmail.com>
2021-02-23 16:12:51 +0300

    delete something

--- app/src/main/java/com/rishav/firebasedemo/StartActivity.java
+++ app/src/main/java/com/rishav/firebasedemo/StartActivity.java
@@ -15,7 +15,6 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class StartActivity extends AppCompatActivity {
 
-    private ImageView iconImage;
     private LinearLayout linearLayout;
     private Button register;
     private Button login;
@@ -25,19 +24,10 @@ public class StartActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-        iconImage = findViewById(R.id.icon_image);
         linearLayout = findViewById(R.id.linear_layout);
         register = findViewById(R.id.register);
         login = findViewById(R.id.login);
 
-        linearLayout.animate().alpha(0f).setDuration(10);
-
-        TranslateAnimation animation = new TranslateAnimation(0 , 0 , 0 , -1500);
-        animation.setDuration(1000);
-        animation.setFillAfter(false);
-        animation.setAnimationListener(new MyAnimationListener());
-
-        iconImage.setAnimation(animation);
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,28 +43,6 @@ public class StartActivity extends AppCompatActivity {
             }
         });
 
-    }
-
-    private class MyAnimationListener implements Animation.AnimationListener {
-
-        @Override
-        public void onAnimationStart(Animation animation) {
-
-        }
-
-        @Override
-        public void onAnimationEnd(Animation animation) {
-
-            iconImage.clearAnimation();
-            iconImage.setVisibility(View.INVISIBLE);
-            linearLayout.animate().alpha(1f).setDuration(1000);
-
-        }
-
-        @Override
-        public void onAnimationRepeat(Animation animation) {
-
-        }
     }
 
     @Override
